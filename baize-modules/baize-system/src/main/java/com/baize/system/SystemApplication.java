a4b6da5906060f381189c43afdbb0aa418aea50f
葛明久 <gemj09@chinatelecom.cn>
2025-01-09 09:28:40 +0800

    更新nacos配置

--- baize-modules/baize-system/src/main/java/com/baize/system/SystemApplication.java
+++ baize-modules/baize-system/src/main/java/com/baize/system/SystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * 白泽代码生成服务启动类
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @MapperScan("com.baize.**.mapper")
 // 表示通过aop框架暴露该代理对象,AopContext能够访问
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableAsync
 @SpringBootApplication
 public class SystemApplication {
     public static void main(String[] args) {
